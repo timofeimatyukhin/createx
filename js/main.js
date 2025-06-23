8f19e246f97f30d4dbf46adecbbd6c02832deca9
Timofey Matyukhin <timtim11111111112@gmail.com>
2025-06-23 21:51:32 +0700

    testimonials in proccess

--- js/main.js
+++ js/main.js
@@ -4,4 +4,36 @@ $(function () {
     $('.directions__filter-btn').removeClass('directions__filter-btn--active')
     $(this).addClass('directions__filter-btn--active')
   })
+
+  $('.team__slider').slick({
+    arrows: false,
+    slidesToShow: 4,
+    infinite: true,
+    draggable: false,
+    waitForAnimate: false,
+  })
+  $('.team__slider-prev').on('click', function (e) {
+    e.preventDefault()
+    $('.team__slider').slick('slickPrev')
+  })
+  $('.team__slider-next').on('click', function (e) {
+    e.preventDefault()
+    $('.team__slider').slick('slickNext')
+  })
+
+  $('.testimonials__slider').slick({
+    arrows: false,
+    // dots: true,
+    infinite: true,
+    draggable: false,
+    waitForAnimate: false,
+  })
+  $('.testimonials__prev').on('click', function (e) {
+    e.preventDefault()
+    $('.testimonials__slider').slick('slickPrev')
+  })
+  $('.testimonials__next').on('click', function (e) {
+    e.preventDefault()
+    $('.testimonials__slider').slick('slickNext')
+  })
 })
